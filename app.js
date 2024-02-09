2a273067961bd33c79fc746fbbb43d0e41b1e2fb
sobolev_aleksei <sobolev.agch@gmail.com>
2024-02-09 13:39:25 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+const TelegramBot = require('node-telegram-bot-api');
+
+app.get('/', (req, res) => {
+    res.send('Hello s8')
+})
+
+app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
+
+// Укажите ваш токен бота
+const token = '6824209048:AAGjezibRtrTdhbc7sO83RQVNf26bb2yTCA';
+
+// Укажите ID канала, куда нужно добавить комментарий
+const channelId = 'JS therapy';
+
+// Создание бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Функция для добавления комментария в канал
+function addComment(comment) {
+    bot.sendMessage(channelId, comment);
+}
+
+// Пример использования функции для добавления комментария
+const commentText = "Всё тлен";
+addComment(commentText);
